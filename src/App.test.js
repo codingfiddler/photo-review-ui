a52bf078409419b4fc6ea5305dcb0ed09f9751a9
Tim Van Cleave <tjvancleave@spscommerce.com>
2020-07-13 18:24:13 -0500

    Updated test

--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,5 @@ test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument();
 });
