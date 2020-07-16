f5fbf1a4f164f9df71a61c83a8bae86c68dcfe3d
Zarrin <zali11@fordham.edu>
2020-07-16 11:44:42 -0400

    fixed user profile

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -34,17 +34,4 @@ class UserProfile extends React.Component {
 
 
 export default UserProfile
-import React from "react";
-import UserNavBar from "../components/userProfile/UserNavBar";
 
-class UserProfile extends React.Component {
-  render() {
-    return (
-      <div>
-        <UserNavBar />
-      </div>
-    );
-  }
-}
-
-export default UserProfile;
