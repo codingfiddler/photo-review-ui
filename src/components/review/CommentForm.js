37c7faf18f6a20e9819bcb13624d09ca14cfe020
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-07-18 06:29:47 -0400

    finally made real-time comments work

--- src/components/review/CommentForm.js
+++ src/components/review/CommentForm.js
@@ -6,49 +6,36 @@ import AssignmentTurnedInSharpIcon from '@material-ui/icons/AssignmentTurnedInSh
 
 class CommentForm extends React.Component {
 
-    state={
-        positiveComment: "",
-        negativeComment: "",
-        name: "Anonymous"
-    }
-
-    handleChange = input => e => {
-        this.setState({ [input]: e.target.value });                                                                                
-    };
-
     render() {
 
-        const {positiveComment, negativeComment, name} = this.state
-
         return (
-            <Container maxWidth="md" style={{borderRadius: '5px', borderStyle: 'solid', borderWidth: '1px', padding: "20px"}}>
+            <Container maxWidth="md" style={{ borderRadius: '5px', borderStyle: 'solid', borderWidth: '1px', padding: "20px" }}>
                 <form onSubmit>
 
-                    Your name: 
+                    {/* Your name: 
                     <input 
                         type="text"
                         placeholder="Please enter your name"
                         onChange={this.handleChange('name')}
                         style={{margin: "10px"}}
-                    />
-                    
+                    /> */}
+
                     {/* <AssignmentTurnedInSharpIcon style={{color: 'green'}}/> */}
-                    <textarea 
+                    <textarea
                         style={{ width: "100%", borderRadius: "5px" }}
                         placeholder="Leave a positive review..."
-                        onChange={this.handleChange('positiveComment')}
                     >
                     </textarea>
 
-                    {/* <AssignmentLateSharpIcon style={{color: 'red'}}/> */}
+                    {/* <AssignmentLateSharpIcon style={{color: 'red'}}/>
                     <textarea 
                         style={{ width: "100%", borderRadius: "5px" }}
                         placeholder="Leave a constructive review..."
                         onChange={this.handleChange('negativeComment')}
                     >
-                    </textarea>
+                    </textarea> */}
 
-                    <Button style={{backgroundColor: "#32e0c4", width: "100%"}}>Submit</Button>
+                    <Button style={{ backgroundColor: "#32e0c4", width: "100%" }}>Submit</Button>
 
                 </form>
             </Container>
