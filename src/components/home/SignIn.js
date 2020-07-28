dfe2650426342f49fe7f9709b4f66f5f1661ca1c
Smilte Valasinaite <smilte.valasinaite@gmail.com>
2020-07-27 21:36:40 -0400

    removed 'remember me' checkbox

--- src/components/home/SignIn.js
+++ src/components/home/SignIn.js
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from 'react-router-dom/Link'
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -72,10 +70,6 @@ export default function SignIn() {
                             type="password"
                             id="password"
                             autoComplete="current-password"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="default" />}
-                            label="Remember me"
                         />
                         <Link to="/explore">
                             <Button
